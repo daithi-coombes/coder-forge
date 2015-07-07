fffacb76c73150849602652716b790dd945237d0
Daithi Coombes <webeire@gmail.com>
2015-07-07 21:16:49 +0100

    3 dataType's

--- intro/app.js
+++ intro/app.js
@@ -1,5 +1,35 @@
+/**
+ * @author daithi coombes <webeire@gmail.com>
+ */
 
+// dataType variable
+var element = "this is an element";
 
-function getElement(element){
-
+// dataType function
+function foo(){
+    element = "foo";
 }
+
+// dataType object
+function fooObj(){
+
+    //private
+    var this_is_internal_variable = 'Hidden inside fooObj()';
+
+    //public - internal function also known as 'method'
+    this.updateElement = function(){
+        element = "fooObj";
+    }
+}
+
+// dataType var's initial state (var element)
+console.log(element);
+
+// call function (function foo())
+foo();
+console.log(element);
+
+// create obj & call method (fooObj.updateElement())
+var myObject = new fooObj();
+myObject.updateElement();
+console.log(element);
